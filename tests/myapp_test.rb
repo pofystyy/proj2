61f3e7eff9045b92f762810eec4d8861219303d1
pofystyy <pofystyy@gmail.com>
2018-11-16 16:05:17 +0200

    update tests

--- tests/myapp_test.rb
+++ tests/myapp_test.rb
@@ -11,14 +11,23 @@ class HomepageTest < Test::Unit::TestCase
     MyApp.new(Main.new)
   end
 
-  def test_response_is_401
+  def test_without_token
     get '/'
+
     assert last_response.status == 401
   end
 
-  def test_response_is_200
-    header  "Authorization", "Bearer "
+  def token_with_the_wrong_secret_key
+    header  "Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.rnOIgq7Ns429jktYhR5anXhGHUBVdElXC5WB2u75jss"
     get '/'
+
+    assert last_response.status == 401
+  end
+
+  def token_with_the_correct_secret_key
+    header  "Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.KsCJ93f7pOyW1CaMCUqtFzVpiX7SaoaOIBqikbe9n_w"
+    get '/'
+
     assert last_response.status == 200
   end
 end
