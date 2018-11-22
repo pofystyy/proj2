a3c9c3a0d649152b194189871b018dc7f9c29c58
pofystyy <pofystyy@gmail.com>
2018-11-22 23:00:19 +0200

    update tests

--- tests/myapp_test.rb
+++ tests/myapp_test.rb
@@ -14,9 +14,14 @@ class HomepageTest < Test::Unit::TestCase
   end
 
   def setup
-    @payload = { "user": 1 }
-    @algo    = 'HS256'
+    @payload = { 'user' => 1 }
     @secret  = "#{ENV["HMAC_SECRET"]}"
+    @algo    = 'HS256'
+
+    @valid_token   = JWT.encode @payload, @secret, @algo
+    @invalid_token = JWT.encode @payload, 'something else', @algo
+
+    @default_url = 'https://resources.com/'
   end
 
   def test_the_path_in_white_list_response_is_200
@@ -32,31 +37,35 @@ class HomepageTest < Test::Unit::TestCase
   end
 
   def test_without_token_response_is_401
-    get 'https://resources.com/'
+    get @default_url
 
     assert_equal 401, last_response.status
   end
 
   def test_availability_of_the_key_in_header
-    token = JWT.encode @payload, @secret, @algo
-    header  'Authorization', "Bearer #{token}"
-    get 'https://resources.com/'
+    header 'Authorization', "Bearer #{@valid_token}"
+    get @default_url
 
-    assert last_response.header.include?('X-Auth-User')
+    assert_equal true, last_response.header.include?('X-Auth-User')
+  end
+
+  def test_definition_of_the_key_correspondence_to_value
+    header 'Authorization', "Bearer #{@valid_token}"
+    get @default_url
+
+    assert_equal @payload, last_response.header['X-Auth-User']
   end
 
   def test_token_with_the_wrong_secret_key_response_is_401
-    token = JWT.encode @payload, 'something else', @algo
-    header  'Authorization', "Bearer #{token}"
-    get 'https://resources.com/'
+    header 'Authorization', "Bearer #{@invalid_token}"
+    get @default_url
 
     assert_equal 401, last_response.status
   end
 
   def test_token_with_the_correct_secret_key_response_is_200
-    token = JWT.encode @payload, @secret, @algo
-    header  'Authorization', "Bearer #{token}"
-    get 'https://resources.com/'
+    header 'Authorization', "Bearer #{@valid_token}"
+    get @default_url
 
     assert_equal 200, last_response.status
   end
